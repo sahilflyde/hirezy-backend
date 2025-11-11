9ed220239fbe6b122b1c1b24b987772132dc733c
Sahil Kalkal <sahil@flyde.in>
2025-11-11 15:46:28 +0530

    socket 2

--- server.js
+++ server.js
@@ -99,7 +99,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:5173", "*"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "*",
+      "https://hirezy-frontend.vercel.app/",
+    ],
     methods: ["GET", "POST"],
   },
 });
